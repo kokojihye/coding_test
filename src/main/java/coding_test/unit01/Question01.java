61122f3db72324bf622b6aadb16784ffbc2b1d96
kokojihye <jihyevivianako@gmail.com>
2024-01-07 15:01:28 +0900

    question1-01.문자 찾기 방법 2추가

--- src/main/java/coding_test/unit01/Question01.java
+++ src/main/java/coding_test/unit01/Question01.java
@@ -23,8 +23,14 @@ public class Question01 {
         str = str.toUpperCase();
         c = Character.toUpperCase(c);
 
-        for (int i=0; i<str.length(); i++){
-            if (str.charAt(i) == c) answer ++;
+        //방법 1
+//        for (int i=0; i<str.length(); i++){
+//            if (str.charAt(i) == c) answer ++;
+//        }
+
+        //방법 2
+        for (char x : str.toCharArray()){
+            if (x == c) answer ++;
         }
         return answer;
     }
