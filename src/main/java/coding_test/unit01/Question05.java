0059a024790cd5e2682c0d6ded9397d975dd4699
kokojihye <jihyevivianako@gmail.com>
2024-01-08 23:16:59 +0900

    question1-05.특정 문자 뒤집기

---
+++ src/main/java/coding_test/unit01/Question05.java
@@ -0,0 +1,49 @@
+package coding_test.unit01;
+
+import java.util.Scanner;
+
+public class Question05 {
+    /**
+     * [문제 5] 특정 문자 뒤집기
+     * 예시 입력, 출력은 Test 코드를 참고한다.
+
+     * 설명
+     * 영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
+     * 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+
+     * 입력
+     * 첫 줄에 길이가 100이 넘지 않는 문자열이 주어집니다.
+
+     * 출력
+     * 첫 줄에 알파벳만 뒤집힌 문자열을 출력합니다
+     */
+    public String solution(String str) {
+        String answer;
+        char[] s = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() -1;
+
+        while(lt<rt){
+            if (!Character.isAlphabetic(s[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(s[rt])) {
+                rt--;
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer = String.valueOf(s);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Question05 question = new Question05();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(question.solution(str));
+    }
+}
